5e07ab89c8876982b97c8406b9fe0f6feaaf8633
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-06 18:17:22 -0500

    commit 007- modulo cliente manage migration y db

--- smotors/clientes/models.py
+++ smotors/clientes/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 class Clientes(models.Model):
     id_cliente = models.AutoField(primary_key=True)
-    cedula_cliente = models.IntegerField(max_length=50)
+    cedula_cliente = models.IntegerField()
     nombre_cliente = models.CharField(max_length=50)
     apellidos_cliente = models.CharField(max_length=50)
     direccion_cliente = models.CharField(max_length=100)
     ciudad_cliente = models.CharField(max_length=50)
     pais_cliente = models.CharField(max_length=50)
-    telefono_cliente = models.IntegerField(max_length=50)
+    telefono_cliente = models.IntegerField()
     email_cliente = models.CharField(max_length=50)
