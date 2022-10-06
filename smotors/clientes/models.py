f0f152da20d49869a67517c491899c85e407f543
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-06 17:59:45 -0500

    commit 006 - file models clintes y setings smotors

--- smotors/clientes/models.py
+++ smotors/clientes/models.py
@@ -1,3 +1,15 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Clientes(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    cedula_cliente = models.IntegerField(max_length=50)
+    nombre_cliente = models.CharField(max_length=50)
+    apellidos_cliente = models.CharField(max_length=50)
+    direccion_cliente = models.CharField(max_length=100)
+    ciudad_cliente = models.CharField(max_length=50)
+    pais_cliente = models.CharField(max_length=50)
+    telefono_cliente = models.IntegerField(max_length=50)
+    email_cliente = models.CharField(max_length=50)
