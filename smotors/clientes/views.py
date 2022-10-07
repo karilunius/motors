217bd508807ac2f2a101900611c2ecaef015a5bd
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-07 15:09:16 -0500

    b-c-commit-creacion de index.html y nav

--- smotors/clientes/views.py
+++ smotors/clientes/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 #@app.route('/ ')
 def  inicio(request):
-        return HttpResponse('<h1>Pagina Principal</h1>')
+        #return HttpResponse('<h1>Pagina Principal</h1>')
+        return render(request, 'index.html')
 
