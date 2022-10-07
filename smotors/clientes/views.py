28fb9ea77c9c05da771730ed4a7fd80162c139c1
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-07 15:18:06 -0500

    c-b-commit-cambio de view con clientes.objects.all

--- smotors/clientes/views.py
+++ smotors/clientes/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Clientes
 # Create your views here.
 #@app.route('/ ')
 def  inicio(request):
         #return HttpResponse('<h1>Pagina Principal</h1>')
-        return render(request, 'index.html')
 
+        clientes = Clientes.objects.all()#se trae todos los registros de la tabla clientes
+        print(clientes)
+        return render(request, 'index.html')
+#no funciono lo de ver el get 
