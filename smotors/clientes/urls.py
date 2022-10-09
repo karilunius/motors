eb9f60a924bb0fd48cd95e6ce0666e749e7c66e3
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-09 13:21:02 -0500

    b-c-commit-creacion dclientes,contactenos viewurls

--- smotors/clientes/urls.py
+++ smotors/clientes/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('crearCliente/', views.crearCliente, name='crearCliente'),
+    path('contactenos/', views.contactenos, name='contactenos'),
+    path('clientes/', views.clientes, name='clientes'),
 ]
 #esta ruta , el nombre se puede conectar al navbar mediante el nombre
