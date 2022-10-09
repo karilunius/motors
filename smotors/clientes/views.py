5817b7ef725ebcf999f2b7b1d42b5690a54b6b29
karilunius <93058053+karilunius@users.noreply.github.com>
2022-10-09 13:21:02 -0500

    b-c-commit-creacion dclientes,contactenos viewurls

--- smotors/clientes/views.py
+++ smotors/clientes/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render, HttpResponse
 from .models import Clientes
 # Create your views here.
 #@app.route('/ ')
-def  inicio(request):
+
+
+def crearCliente(request) :
+        return render(request, 'crearCliente.html')
+
+
+def contactenos(request) :
+        return render(request, 'contactenos.html')
+
+
+def inicio(request) :
+        return render(request, 'index.html')
+
+def  clientes(request):
         #return HttpResponse('<h1>Pagina Principal</h1>')
 
         clientes = Clientes.objects.all()#se trae todos los registros de la tabla clientes
@@ -10,9 +23,5 @@ def  inicio(request):
         contexto = {
                 'clientes' : clientes
         }
-        return render(request, 'index.html', contexto)
-#no funciono lo de ver el get 
-
-
-def crearCliente(request) :
-        return render(request, 'crearCliente.html')
+        return render(request, 'clientes.html', contexto)
+#no funciono lo de ver el get  luego de reiniciar sí funcionó
